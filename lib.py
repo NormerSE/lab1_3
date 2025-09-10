275073daa8d4c0d60d0b462b8e81dc1b1a0937d0
NormerSE <normer3500@gmail.com>
2025-09-10 14:17:03 +0500

    Отредактировал файл

--- lib.py
+++ lib.py
@@ -7,6 +7,3 @@ def count_common_elements(*lists):
     common_elements = set.intersection(*sets)
     return len(common_elements)
 # Пример
-print(count_common_elements([1, 2, 3, 4],[2, 3, 5],[0, 2, 3, 7]))
-# Ответ: 2 (общие элементы 2 и 3)
-input()
